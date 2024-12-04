26a52a329831492f34f8a871c01d4115b227d476
Ferjose007 <167478921+Ferjose007@users.noreply.github.com>
2024-12-04 00:36:14 -0500

    tercer cambio

---
+++ src/menu.php
@@ -0,0 +1,25 @@
+<header class="bg-dark text-white p-4 shadow-sm text-center">
+
+    <div class="logo">
+        <a href="/02_proyectoua/src/index.php" class="titulo-link text-white text-decoration-none">
+            <h1 class="fs-1 fw-bold">Dilo fuerte Autónomo</h1>
+        </a>
+        <p class="eslogan fs-4">Las noticias más relevantes al instante</p>
+    </div>
+    <nav>
+        <ul class="nav justify-content-center">
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/politica.php" class="nav-link text-white">Política</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/nacional.php" class="nav-link text-white">Nacional</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/internacional.php" class="nav-link text-white">Internacional</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/sociedad.php" class="nav-link text-white">Sociedad</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/policial.php" class="nav-link text-white">Policial</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/cultura.php" class="nav-link text-white">Cultura</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/deporte.php" class="nav-link text-white">Deportes</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/economia.php" class="nav-link text-white">Economía</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/tecnologia.php" class="nav-link text-white">Tecnología</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/arte.php" class="nav-link text-white">Arte</a></li>
+            <li class="nav-item"><a href="/02_proyectoua/src/mvc/vista/menu/espectaculos.php" class="nav-link text-white">Espectáculos</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-white">Entretenimiento</a></li>
+        </ul>
+    </nav>
+</header>
